740dc1eede5b70cb1c87a8cb2c9a393fbe1b2521
ipanfilii <panfiliiion@yahoo.com>
2017-05-28 20:14:45 +0300

    Add files via upload

---
+++ hotels_list_reqData.php
@@ -0,0 +1,35 @@
+<?php
+  header('Access-Control-Allow-Origin:*'); 
+  header("Access-Control-Allow-Credentials: true");
+  header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+  header('Access-Control-Max-Age: 100000');
+  header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization','Content-Type: application/json; charset=utf-8');
+  header('Access-Control-Allow-Headers: origin, X-Requested-With, Authorization, Content-Type');
+  header('Content-type: application/json');
+  
+   $db = new mysqli('localhost','root','' );
+
+    //verify connection
+    if($db->connect_errno > 0) {   
+      die('Unable to connect to database [' . $db->connect_error . ']');
+    } else {
+        //connected
+    }
+
+      $query = 'SELECT * FROM `myplaces`.`hotels` WHERE 1 ';
+      if(!$result = $db->query($query))
+		{
+			die('There was an error running the query [' . $db->error . ']');
+		}
+	
+	while($row = $result->fetch_array())
+		{
+			$hotels_list[] = array(
+				'id' => $row['id'],
+				'name' => $row['name'],
+				'address' => $row['address'],
+				'username' => $row['username']
+			);	
+		}
+		echo json_encode($hotels_list);
+?>
---
+++ room_type_reqData.php
@@ -0,0 +1,44 @@
+<?php
+  header('Access-Control-Allow-Origin:*'); 
+  header("Access-Control-Allow-Credentials: true");
+  header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+  header('Access-Control-Max-Age: 100000');
+  header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization','Content-Type: application/json; charset=utf-8');
+  header('Access-Control-Allow-Headers: origin, X-Requested-With, Authorization, Content-Type');
+  header('Content-type: application/json');
+  
+   $postdata = file_get_contents("php://input");
+   $db = new mysqli('localhost','root','' );
+
+    //verify connection
+    if($db->connect_errno > 0) {   
+      die('Unable to connect to database [' . $db->connect_error . ']');
+    } else {
+        //connected
+    }
+
+	
+	if( $postdata){
+	$data = json_decode($postdata);
+	 if( $data->hotelid != null)
+		{
+			
+						$query = 'SELECT * FROM `myplaces`.`room_types` WHERE hotelID = "'.$data->hotelid.'" ;';
+						if(!$result = $db->query($query))
+							{
+								die('There was an error running the query [' . $db->error . ']');
+							}
+		}
+	while($row = $result->fetch_array())
+		{
+			$types[] = array(
+				'name' => $row['name'],
+				'short_name' => $row['short_name'],
+				'base_price' => $row['base_price'],
+				'max_occupancy' => $row['max_occupancy'],
+				'base_availability' => $row['base_availability']
+			);	
+		}
+		echo json_encode($types);
+		}
+?>
